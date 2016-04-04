68033b6892270a81b7a6513c162d84558cc211ec
Julian Goacher <julian.goacher@innerfunction.com>
2016-04-04 22:58:56 +0100

    New view layout loading behaviour.

--- SemoCore/com_innerfunction_semo_ui/IFViewController.h
+++ SemoCore/com_innerfunction_semo_ui/IFViewController.h
@@ -27,9 +27,32 @@
  * Implements the <IFMessageReceiver> protocol and responds to the following messages:
  * - _toast_: Display an Android style popup message. Must have a _message_ parameter.
  * - _show-image_: Display an image in a pop-over view. Must have an _image_ parameter.
+ *
+ * This class supports loading of view layouts from XIB files, and configuration of view
+ * components contained by such layouts. This can be done in a couple of ways.
+ *
+ * Method one utilizes the container's ability to perform configuration of in-place
+ * property values. To use this method, this class should be subclassed with new properties
+ * representing the configurable view components. The properties should be marked using the
+ * IBOutlet attribute and then be used as referencing outlets within the XIB. When the
+ * new view controller class is instantiated by a container, the layout will be loaded and
+ * the view components mapped to their respective properties before the container performs
+ * its IOC configuration on the controller instance, at which point it can inject any
+ * additional configuration values into the view component properties.
+ *
+ * The second method involves creating empty views as placeholders within the layout, and
+ * mapping these placeholders to named referencing outlets. The actual view instances can
+ * then be defined under the _namedViews_ collection property of the controller. After the
+ * layout is loaded, the controller will look for placeholders for each of its named views,
+ * and replace each placeholder with the named view instance. This can be done for both
+ * UIView and UIViewController instances. This second method doesn't necessarily require
+ * this class to be subclassed, so long as the referencing outlets are named appropriately
+ * in the layout file.
  */
 @interface IFViewController : UIViewController <IFIOCContainerAware, IFMessageReceiver, IFActionProxy, IFViewBehaviourController> {
     NSMutableDictionary *_actionProxyLookup;
+    BOOL _loadingLayout;
+    NSMutableDictionary *_namedViewPlaceholders;
 }
 
 /** Flag indicating whether to show or hide the title bar. */
@@ -50,10 +73,23 @@
  * built from the configurations described in this map.
  */
 @property (nonatomic, strong) NSDictionary *namedViews;
-/** Map of named view names onto nib file view tags. */
-@property (nonatomic, strong) NSDictionary *namedViewTags;
+
 
 - (id)initWithView:(UIView *)view;
+/// Post a message.
 - (void)postMessage:(NSString *)message;
+/**
+ * Load the view's layout. The object should be configured with a _layoutName_ value, which it will use
+ * as the name of a XIB file to load the layout from.
+ */
+- (void)loadLayout;
+/**
+ * Insert named views into the layout. Inserts any view components declared using the _namedViews_
+ * property into the layout by finding a placeholder view bound to a referencing outlet of the same
+ * name, and replacing it with the named view.
+ */
+- (void)insertNamedViews;
+/// Replace one of the view's subviews with a new view.
+- (void)replaceSubview:(UIView *)view withView:(UIView *)view;
 
 @end
